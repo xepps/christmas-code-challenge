fd5dba9ff5d59a74857900c36d0808609bacb3eb
Kristian Epps <kristian.epps@bbc.co.uk>
2015-12-10 13:41:52 +0000

    Bootstrap code for challenge 1

---
+++ 1/index.js
@@ -0,0 +1,10 @@
+function getInput() {
+    return {
+        "numberOfPeople": process.argv[2],
+        "numberOfFruits": process.argv[3]
+    };
+}
+
+function outputAnswer(answer) {
+    console.log(answer);
+}
